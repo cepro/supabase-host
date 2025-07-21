8b6f3c6541bc83d11a6408adbbb2a1478a5750b0
Chris Hatch <devcebfaff@proton.me>
2025-07-21 14:43:34 +1000

    split out passwords - multi were pointing at 'pgpass' add bec target

--- docker/volumes/db/roles.sql
+++ docker/volumes/db/roles.sql
@@ -1,8 +1,8 @@
--- NOTE: change to your own passwords for production environments
-\set pgpass `echo "$POSTGRES_PASSWORD"`
+\set authenticator_password `echo "$AUTHENTICATOR_PASSWORD"`
+\set supabase_auth_admin_password `echo "$SUPABASE_AUTH_ADMIN_PASSWORD"`
 
-ALTER USER authenticator WITH PASSWORD :'pgpass';
-ALTER USER pgbouncer WITH PASSWORD :'pgpass';
-ALTER USER supabase_auth_admin WITH PASSWORD :'pgpass';
-ALTER USER supabase_functions_admin WITH PASSWORD :'pgpass';
-ALTER USER supabase_storage_admin WITH PASSWORD :'pgpass';
+ALTER USER authenticator WITH PASSWORD :'authenticator_password';
+ALTER USER supabase_auth_admin WITH PASSWORD :'supabase_auth_admin_password';
+-- ALTER USER pgbouncer WITH PASSWORD :'pgpass';
+-- ALTER USER supabase_functions_admin WITH PASSWORD :'pgpass';
+-- ALTER USER supabase_storage_admin WITH PASSWORD :'pgpass';
--- sqitch/deploy/0006_roles.sql
+++ sqitch/deploy/0006_roles.sql
@@ -2,10 +2,10 @@
 
 BEGIN;
 
-ALTER USER authenticator WITH PASSWORD :'pgpass';
+ALTER USER authenticator WITH PASSWORD :'authenticator_password';
+ALTER USER supabase_auth_admin WITH PASSWORD :'supabase_auth_admin_password';
 -- ALTER USER pgbouncer WITH PASSWORD :'pgpass';  drop this because Timescale cloud already has a session pooler
-ALTER USER supabase_auth_admin WITH PASSWORD :'pgpass';
-ALTER USER supabase_functions_admin WITH PASSWORD :'pgpass';
+-- ALTER USER supabase_functions_admin WITH PASSWORD :'pgpass';
 -- ALTER USER supabase_storage_admin WITH PASSWORD :'pgpass'; Storage functionality is not currently used so this has been disabled
 
 COMMIT;
