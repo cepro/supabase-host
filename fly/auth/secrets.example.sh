0819e4e21e8cdd9953df6766557d3459b9c947aa
Chris Hatch <devcebfaff@proton.me>
2025-07-28 13:29:02 +1000

    update SMTP settings - added a server but actually disabled by GOTRUE_MAILER_AUTOCONFIRM=true for now

--- fly/auth/secrets.example.sh
+++ fly/auth/secrets.example.sh
@@ -1,2 +1,3 @@
 fly --config fly-auth.toml secrets set GOTRUE_DB_DATABASE_URL=""
 fly --config fly-auth.toml secrets set GOTRUE_JWT_SECRET=""
+fly --config fly-auth.toml secrets set GOTRUE_SMTP_PASSWORD=""
